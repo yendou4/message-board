900b7bc7594b9701fddaa42f0552e694528c727f
yendou4 <in_japan_yuen@i.softbank.jp>
2015-12-03 09:33:59 +0000

    mod index

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ２文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  # 年齢は必須入力かつ０以上の数字
+  validates :age , numericality: {greater_than_or_equal_to:0, only_integer:true} , presence: true
 end
